1ebe6afefe8b1a8370f1e110678845f1cf13145c
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-14 16:59:58 -0500

    auto-populate option tags

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     // let body = document.querySelector('body')
     // let select = document.createElement('select')
+    // let options = document.createElement('option')
     // body.appendChild(select)
-
     
-   
+    
+
+getClues()
 })
 
 let baseUrl = 'http://jservice.io'
 
-fetch(`${baseUrl}/api/clues`)
+let select= document.createElement('select')
+document.querySelector('body').appendChild(select)
+// let option = document.createElement('option')
+
+
+const getClues = (event) => {fetch(`${baseUrl}/api/clues`)
          .then(response => {
              return response.json()
          }).then((data) => {
-             console.log(data) 
+             console.log(data)
+            let options = data.map(el => {
+                return `<option value=${el.category.title}>${el.category.title}</option>`
+            })
+            select.innerHTML = options
      })
-
+    }
 
     
 
