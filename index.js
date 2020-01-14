e54da1aa91181b7ac5ac8251d2576c71ace94aa0
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-14 13:23:08 -0500

    make request to jservice api

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let body = document.querySelector('body')
-    let select = document.createElement('select')
-    body.appendChild(select)
+    // let body = document.querySelector('body')
+    // let select = document.createElement('select')
+    // body.appendChild(select)
 
-    let baseUrl = 'http://jservice.io'
-
-
+    
+   
 })
+
+let baseUrl = 'http://jservice.io'
+
+fetch('https://ghibliapi.herokuapp.com/films')
+         .then(response => {
+             return response.json()
+         }).then((data) => {
+             console.log(data)
+     })
+
+
+    
+
