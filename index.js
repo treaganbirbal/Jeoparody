51c63103bffa655c6239acc449de1ad9390fb535
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-14 22:36:55 -0500

    display clue text of selected question

--- index.js
+++ index.js
@@ -6,9 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
     selector.classList.add('movie-selector')
     let baseUrl = 'http://jservice.io'
 
+    let clueObj = {}
+
 document.querySelector('body').appendChild(selector)
 let option = document.createElement('option')
 
+selector.addEventListener("change", (event) => {
+    displayClue(clueObj[event.target.value])
+    // debugger
+})
+
+const displayClue = (clue) => {
+    let main = document.querySelector('main')
+    let oldClue = document.querySelector('.clue-display')
+    let newClue = document.createElement('div')
+    newClue.classList.add('clue-display')
+    let clueTag = document.createElement('h3')
+    clueTag.innerText = clueObj[clue.id].question
+    newClue.appendChild(clueTag)
+    main.replaceChild(newClue, oldClue)
+}
 
 const fireRequest = (url, callback) => {
     fetch(url).then(res =>  res.json().then(data => callback(data)))
@@ -16,6 +33,8 @@ const fireRequest = (url, callback) => {
 
 const updateSelect = (clues) => {
     clues.forEach(clue => {
+        clueObj[clue.id] = clue
+        console.log(clueObj)
         let catOption = document.createElement('option')
         catOption.innerText = clue.category.title
         catOption.value = clue.id
