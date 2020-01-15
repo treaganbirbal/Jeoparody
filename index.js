80677f1f44b8be82cc17cf29dff09d39fa104bec
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-14 19:35:57 -0500

    midst of refactoring

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // let body = document.querySelector('body')
-    // let select = document.createElement('select')
-    // let options = document.createElement('option')
+    let body = document.querySelector('body')
+    let select = document.createElement('select')
+    let options = document.createElement('option')
     // body.appendChild(select)
     
     
 
-getClues()
-})
+
 
 let baseUrl = 'http://jservice.io'
 
-let select= document.createElement('select')
 document.querySelector('body').appendChild(select)
-// let option = document.createElement('option')
+let option = document.createElement('option')
 
 
-const getClues = (event) => {fetch(`${baseUrl}/api/clues`)
-         .then(response => {
-             return response.json()
-         }).then((data) => {
-             console.log(data)
-            let options = data.map(el => {
-                return `<option value=${el.category.title}>${el.category.title}</option>`
-            })
-            select.innerHTML = options
-     })
-    }
+const fireRequest = () => {
+    // fetch(`${baseUrl}/api/clues`)
+    console.log('hello')
+}
+
+const updateSelect = (movies) => {
+    debugger
+}
+})
+
 
     
 
